40db50cae9954d6af3a322803225eb1a712fcbaf
Matthew Sah <mattsah2001@gmail.com>
2024-08-06 22:31:27 -0700

    more refactoring - split into main.rs and lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,21 @@
 use std::env;
-use std::fs;
+use std::process;
+use rust_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let config: Config = Config::new(&args);
-    
+    // what is unwrap or else ||
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-    
-    let contents: String = fs::read_to_string(config.file_path)
-        .expect("Unable to read file");
-    println!("Got contents: {contents}")
-}
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query: String = args[1].clone();
-        let file_path: String = args[2].clone();
-
-        Config { query, file_path }
+    // error handling - we don't need the result returned from Ok
+    if let Err(e) = rust_grep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
