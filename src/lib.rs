6fd6808eec4efd639a6db64f0eb9027b73d386d9
Matthew Sah <mattsah2001@gmail.com>
2024-08-07 15:11:18 -0700

    implement functionality

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,39 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents: String = fs::read_to_string(config.file_path)
-        .expect("Unable to read the file");
-    println!("File has text:\n{contents}");
+    let contents: String = fs::read_to_string(config.file_path).expect("Unable to read the file");
+    
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,7 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
+    
     // error handling - we don't need the result returned from Ok
     if let Err(e) = rust_grep::run(config) {
         println!("Application error: {e}");
