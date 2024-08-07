506f8181e8c354cf18f62be2e62cd9adc3dca15d
Matthew Sah <mattsah2001@gmail.com>
2024-08-06 21:48:20 -0700

    basic cli input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let query: &str = &args[1];
+    let file_path: &str = &args[2];
+
+    println!("Searching for {query}");
+    println!("in file {file_path}");
 }
